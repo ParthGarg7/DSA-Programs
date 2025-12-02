ea20526089925402ed345eec4be1bfe39ad3d3f5
Parth Garg <parthgarg750@gmail.com>
2025-12-02 08:09:35 +0530

    Linklist

---
+++ DSA in CPP/Linklist/4_Circular_Linked_List_Traversal.cpp
@@ -0,0 +1,19 @@
+// // https://www.geeksforgeeks.org/problems/circular-linked-list-traversal/1
+
+// /* structure for a node
+// struct Node
+// {
+//     int data;
+//     struct Node *next;
+// }; */
+
+// /* Function to print nodes in a given Circular linked list */
+// void printList(struct Node *head)
+// {
+//     struct Node *temp = head;
+//     do
+//     {
+//         cout << temp->data << " ";
+//         temp = temp->next;
+//     } while (temp != head);
+// }
